10627837aa43842b3371728999698c6805eb1f46
Fanny <oeilden8@gmail.com>
2021-11-25 14:50:15 +0100

    finished home header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  return <div></div>;
+  return (
+    <div className="header-container">
+      <header>
+        <Link to="/mood">
+          <button type="button" className="button-mood"></button>{" "}
+        </Link>
+        <Link to="/party">
+          <button type="button" className="button-party"></button>
+        </Link>
+        <Link to="/contact">
+          <button type="button" className="button-contact"></button>
+        </Link>
+      </header>
+    </div>
+  );
 }
