82ef8d4e2577416b6a5bce137e129539e8b4db1a
EnzoFuche <fuche.enzo@gmail.com>
2021-11-25 16:20:08 +0100

    Background gradient + début footer img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import equalizer from "./assets/background-01.png";
 
 function App() {
   return (
     <div className="App">
-     
+      <footer className="footer">
+        <img src={equalizer} alt="Equalizer" id="equa" />
+      </footer>
     </div>
   );
 }
