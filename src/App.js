cb0be533711896b1b155252744a272e5755e19ea
Fanny <oeilden8@gmail.com>
2021-11-25 13:50:57 +0100

    created Header and Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import HeaderHome from "./components/HeaderHome";
 
 function App() {
   return (
     <div className="App">
-     
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <HeaderHome />
+            </div>
+          }
+        />
+        <Route
+          path="/mood"
+          element={
+            <div>
+              <Header />
+            </div>
+          }
+        />
+        <Route
+          path="/party"
+          element={
+            <div>
+              <Header />
+            </div>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <div>
+              <Header />
+            </div>
+          }
+        />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/HeaderHome.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Header.css";
+
+export default function HeaderHome() {
+  return <div></div>;
+}
