c7a532463aa153ddfd95df8a8b9791171810e284
Dieumegard <dieumegard.gregory@gmail.com>
2021-11-25 16:40:17 +0100

    fetched data from the API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
+import MoodSelector from './components/MoodSelector/MoodSelector';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <MoodSelector />
     </div>
   );
 }
