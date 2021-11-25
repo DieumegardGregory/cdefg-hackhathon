e93202e5d38e75b0b3777a130e48cc0555da6931
Davy <davy.oger@yahoo.fr>
2021-11-25 16:05:50 +0100

    création cartes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import CardList from "./components/CardList/CardList";
 
 function App() {
   return (
     <div className="App">
-     
+      <CardList />
     </div>
   );
 }
