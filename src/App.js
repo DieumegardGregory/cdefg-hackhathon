35154ed992f7652243e3f69e462f36dfa6adec9b
EnzoFuche <fuche.enzo@gmail.com>
2021-11-25 18:01:33 +0100

    Equalizer responsive fait

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import equalizer from "./assets/background-01.png";
+import equalizerMobile from "./assets/background-mobi-01.png";
 
 function App() {
   return (
     <div className="App">
       <div className="relative">
         <img src={equalizer} alt="Equalizer" id="equa" />
+        <img src={equalizerMobile} alt="Equalizer" id="equalizerMobile" />
       </div>
     </div>
   );
